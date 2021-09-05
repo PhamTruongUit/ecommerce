a557d86df79e8b62f582189d78ed6b551b163708
PhamTruong <18521571@gm.uit.edu.vn>
2021-09-05 15:53:38 +0700

    debug add cart

--- fe/main.js
+++ fe/main.js
@@ -1,31 +1,60 @@
 window.onload = function() {
+    listenEven()
+    function listenEven(){
+        const temp = []
+        const buyButton = document.querySelectorAll('.buyButton')
+        const closeButton = document.querySelector('.closeButton')
+        const shoppingButton = document.querySelector('.shoppingButton')
+        const popUp = document.querySelector('.popUp')
+        const box = document.querySelector('.box')
+
+        // add obj
+        buyButton.forEach((e)=> {
+            e.addEventListener('click',() => {
+                const id = e.parentElement.id
+                temp.push(id)
+            })  
+        })
+
+        //cart-shop
+        shoppingButton.addEventListener('click',() => {
+            box.style.display = 'flex'
+            box.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'
+            popUp.style.display = 'block'
+            setdata(temp)
+        })
+
+        //close pop-up
+        closeButton.addEventListener('click',() => {
+            box.style.display = 'none'
+            box.style.backgroundColor = 'none'
+            popUp.style.display = 'none'
+        })
+    }
+
+    //test
+    // box.style.display = 'flex'
+    // box.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'
+    // popUp.style.display = 'block'
+    setdata(['01','02','03','02'])
     
-    let pre_keys = []
-
-    const server = "http://localhost:3000"
-    const buyButton = document.querySelectorAll('.buyButton')
-    const closeButton = document.querySelector('.closeButton')
-    const shoppingButton = document.querySelector('.shoppingButton')
-    const popUp = document.querySelector('.popUp')
-    const box = document.querySelector('.box')
-
-    // fetch data from server
-    function getdata (pre_keys) {
+    // fetch data cart from server
+    function setdata (temp) {
+        const keys = Array.from(new Set(temp))
+        const count = count_obj(keys,temp)
+        const server = "http://localhost:3000"
         let info = []
-        let keys = Array.from(new Set(pre_keys))
         keys.forEach((id)=>{
-            const obj = server + '/' + id
-            fetch(obj)
+            fetch(server + '/' + id)
                 .then ((rep) => rep.json())
                 .then ((obj) => info.push(obj))
         })
-        let count = count_obj(keys,pre_keys)
-        return { info, count }
+        return addCart({ info, count })
     }
 
     // count obj
     function count_obj(keys,pre_keys){
-        let count = []
+        const count = []
         keys.forEach(id => {
             let sum = 0
             pre_keys.forEach(obj => {
@@ -36,28 +65,51 @@ window.onload = function() {
         return count
     }
 
-    // add obj
-    buyButton.forEach((e)=> {
-        e.addEventListener('click',() => {
-            const id = e.parentElement.id
-            pre_keys.push(id)
-        })  
-    })
+    function addCart(obj) {
+        console.log(obj)
+        console.log(obj.info)
+        console.log(obj.info[0])
+        console.log(obj.info.length)
+        for (let i = 0; i<obj.info.length; ++i)
+            {   
+                let url = obj.info[i].url
+                let name = obj.info[i].name
+                let price = obj.info[i].price
+                let count = obj.count[i]
+                render(url, name, price, count)
+            }
+        // debug
+        // console.log(obj)
+    }
 
-    //cart-shop
-    shoppingButton.addEventListener('click',() => {
-        box.style.display = 'flex'
-        box.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'
-        popUp.style.display = 'block'
-        console.log(getdata(pre_keys))
-    })
+    // addCart
+    function render(url, name, price,count) {
+        const cart = document.querySelector('.cart')
+        const sum = document.querySelector('.sum')
+        addElementBase()
+    }
 
-    //close pop-up
-    closeButton.addEventListener('click',() => {
-        box.style.display = 'none'
-        box.style.backgroundColor = 'none'
-        popUp.style.display = 'none'
-    })
+    function addElementBase(target, nameElement, attribute, style) {
+        // attribute and style are array 2d
+        try {
+            const [parent, child] = target
+            let base = document.createElement(nameElement)
+            
+            attribute.forEach((node) => {
+                const [property, value] = node
+                base.setAttribute(property, value)
+            })
+
+            style.forEach((node)=>{
+                const [property, value] = node
+                base.style[property] = value
+            })
+            parent.insertBefore(base,child)
+        }
+        catch (err) {
+            return console.error('err render')
+        } 
+    }
 
     // remove, add and subtract with default value = 1
     function fix(obj, id, type) {
